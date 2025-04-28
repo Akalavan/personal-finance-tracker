431301b7a93f585137f6b5f3541fad49c528442c
Vagin <vagin@skytecgames.com>
2025-04-28 19:37:15 +0500

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,33 @@
 plugins {
-    kotlin("jvm") version "2.0.0" apply false
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
     id("org.springframework.boot") version "3.2.5" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
 }
 
 allprojects {
-    group = "com.personalfinance"
+    group = "com.akalavan"
     version = "0.0.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
     }
 }
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    }
+}
+
 
